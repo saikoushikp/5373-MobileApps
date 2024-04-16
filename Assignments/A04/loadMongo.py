6423a2630796c790d74020a9ed9e650ee9927e1a
saikoushikp <75295510+saikoushikp@users.noreply.github.com>
2024-04-16 15:34:09 -0500

    Update loadMongo.py

--- Assignments/A04/loadMongo.py
+++ Assignments/A04/loadMongo.py
@@ -23,30 +23,39 @@ def convert_jpg_to_png_in_memory(jpg_path):
         in_memory_file.seek(0)
         return in_memory_file.getvalue()
     
-
-def load():
+def load(**kwargs):
     json_files = glob.glob("./categoryJson/*.json")
 
+    username = kwargs.get("username", None)
+    password = kwargs.get("password", None)
+    db = kwargs.get("db", None)
+    collection1 = kwargs.get("collection1", None)
+    collection2 = kwargs.get("collection2", None)
+
+    print(collection1)
+    print(collection2)
+
+    # db1 = MongoManager(username=username, password=password, db=db)
+    # db2 = MongoManager(username=username, password=password, db=db)
+
     db = MongoManager()
+    # db2 = MongoManager()
 
-    db.setDb("candy_store_2")
+    db.setDb("candy_store")
 
-    db.dropCollection('candies')
+    db.dropCollection("candies")
 
-    db.dropCollection('categories')
+    db.setCollection("categories")
 
-    db.dropCollection('images')
+    db.dropCollection("categories")
 
+    db.dropCollection("images")
 
-    category_id = 0
-    candy_id = 0
-    candy_long_ids = []
-    big_candy_dict = {}
+    i = 0
     for file in json_files:
+
         print(file)
         parts = file.split("/")
-        print("parts",parts)
-        image_folder = parts[-1][:-5]
         category = parts[-1][:-5].replace("-", " ").title()
 
         print(category)
@@ -58,58 +67,27 @@ def load():
 
             summary["count"] = len(data)
             summary["name"] = category
-            summary["_id"] = category_id
-            summary["candies"] = []
-            category_id += 1
+            summary["_id"] = i
 
-            # iterate over the candy items in each of the category files
             for id, item in data.items():
-                if not 'categories' in item:
-                    item["categorys"] = []
-
-                # if first time seeing this candy add its id to a list
-                if not id in candy_long_ids:
-                    candy_long_ids.append(id)
-                else:
-                    # get the index location of the original candy id
-                    print(f"id: {id} exists at {index}! "+"="*20)
-                    
-
-                index = candy_long_ids.index(id)
-
-                if not index in big_candy_dict:
-                    item["_id"] = index
-                    big_candy_dict[index] = item
-                    summary["candies"].append(index)
-                    item['img_path'] = "./images/"+image_folder+"/"+id+'.jpg'
-                    print(item['img_path'])
-            
-                big_candy_dict[index]["categorys"].append(category_id)
-
-                #print(item)
-                # db.setCollection("candies")
-                # db.post(item)
-        db.setCollection('categories')
+                item["category"] = category
+                item["category_id"] = i
+                print(item)
+                db.setCollection("candies")
+                db.post(item)
+        db.setCollection("categories")
         db.post(summary)
-        candy_id += 1
+        i += 1
 
-    db.setCollection("candies")
-    for id,item in big_candy_dict.items():
-        db.post(item)
-        
-
-'''
-    for id,item in big_candy_dict.items():
-        
-        png_data = convert_jpg_to_png_in_memory(item['img_path'])
-        db.store_image_in_mongodb(id,png_data)
-
-    
-    print(len(big_candy_dict))
-    with open("big_updated_candy.json","w") as f:
-        json.dump(big_candy_dict,f)
-'''
 
 if __name__ == "__main__":
 
-    load()
+    kwargs = {
+        "username": "knreddy",
+        "password": "HappyBirthday",
+        "db": "candy_store",
+        "collection1": "candies",
+        "collection2": "categories",
+    }
+
+    load(**kwargs)
